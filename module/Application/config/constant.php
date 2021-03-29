57c2fe3ea75e492778efb19222a776f11f88b529
root <root@instance-1.us-central1-a.c.esoteric-scarab-286107.internal>
2021-03-29 12:51:35 +0000

    setup

--- module/Application/config/constant.php
+++ module/Application/config/constant.php
@@ -1,11 +1,11 @@
 <?php
 $GLOBALS['HTTP_SITE_ADMIN_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/admin/';
-$GLOBALS['SITE_APP_URL'] = 'http://'.$_SERVER['SERVER_NAME'].'/frontend/application/index';
+$GLOBALS['SITE_APP_URL'] = 'http://'.$_SERVER['SERVER_NAME'].'/application/index';
 $GLOBALS['SITE_COMPANY_URL'] = 'http://172.104.239.54/accrabasket/merchant/';
 $GLOBALS['PAGE_BEFORE_LOGIN'] = array('Admin\Controller\Index\login','Admin\Controller\Index\index');
 $GLOBALS['SITE_PATH'] = $_SERVER['DOCUMENT_ROOT'];
 define('NODE_API', 'http://172.104.239.54:3000/');
-define('BASKET_API', 'http://'.$_SERVER['SERVER_NAME'].'/frontend/basketapi/index.php/');
+define('BASKET_API', 'http://'.$_SERVER['SERVER_NAME'].'/basketapi/index.php/');
 $GLOBALS['PRODUCTIMAGEPATH'] = $_SERVER['DOCUMENT_ROOT'].'accrabasket/product_img';
 $GLOBALS['ATTRIBUTEIMAGEPATH'] = $_SERVER['DOCUMENT_ROOT'].'/accrabasket/attribute_img';
 $GLOBALS['LEFT_MENU_ALLOW'] = array('product','hotdeals');
